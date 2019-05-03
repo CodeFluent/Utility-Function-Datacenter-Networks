a65c2a2b2372e1fee35387cca048baf246e4a9f2
CodeFluent <wasfimomenwim@gmail.com>
2019-05-03 07:58:06 -0400

    adding new file and cvxopt

---
+++ barrier.py
@@ -0,0 +1,21 @@
+
+from cvxopt import lapack, solvers, matrix, spdiag, log, div, normal
+from cvxopt.modeling import variable, op, max, min, sum
+
+
+c1 = []  # input data size
+c2 = []  # link bandwidth
+c3 = []  # execution time
+c4 = []  # communication time
+c5 = []  # resource capacity
+
+
+# %%
+
+def centering():
+    pass
+
+
+# %%
+def solve():
+    pass
